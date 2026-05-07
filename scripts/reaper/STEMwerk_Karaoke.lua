1824a9cb70c07d9495770ed497fa9e4e202bfcf8
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-07 06:23:31 +0200

    chore: bump version to v2.2.2.1

--- scripts/reaper/STEMwerk_Karaoke.lua
+++ scripts/reaper/STEMwerk_Karaoke.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Karaoke
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.0
+-- @version 2.2.2.1
 -- @changelog
 --   Quick preset: vocals removed.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
