641a6e35322f4ff88a7ebb43b93d57e67a4b7110
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-10 01:10:28 +0200

    reapack: publish macOS MPS Auto hotfix 2.2.2.1.8

--- scripts/reaper/STEMwerk_Explode_Takes.lua
+++ scripts/reaper/STEMwerk_Explode_Takes.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Explode Takes (In Place)
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.1.7
+-- @version 2.2.2.1.8
 -- @changelog
 --   Quick action: explode selected multi-take items in place.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
