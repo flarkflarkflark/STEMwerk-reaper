1260e42ff9eb7ef91f42442e5fbccdd1a7fa6366
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-09 18:29:49 +0200

    reapack: publish macOS NumPy hotfix 2.2.2.1.5

--- scripts/reaper/STEMwerk_Vocals_Only.lua
+++ scripts/reaper/STEMwerk_Vocals_Only.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Vocals Only
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.1.4
+-- @version 2.2.2.1.5
 -- @changelog
 --   Quick preset: vocals only.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
