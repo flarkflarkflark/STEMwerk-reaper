b2af953ffe870eaadc6566fa5d8c84124af85d7a
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-09 19:09:22 +0200

    reapack: publish macOS assertion hotfix 2.2.2.1.6

--- scripts/reaper/STEMwerk_Setup_Toolbar.lua
+++ scripts/reaper/STEMwerk_Setup_Toolbar.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Setup Toolbar
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.1.5
+-- @version 2.2.2.1.6
 -- @changelog
 --   Adds/refreshes STEMwerk scripts in the Action List and guides toolbar setup.
 --   Registers the dedicated "Stemwerk: Explode Takes (In Place)" quick action.
