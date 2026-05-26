a5f0d92fe1924006f7809ae763d242655ef693de
flarkAUDIO <69244166+flarkflarkflark@users.noreply.github.com>
2026-05-26 18:21:55 +0200

    reapack: restore public hotfix line 2.2.2.2.5 (#51)

--- scripts/reaper/STEMwerk_Vocals_Only.lua
+++ scripts/reaper/STEMwerk_Vocals_Only.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Vocals Only
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.2.4
+-- @version 2.2.2.2.5
 -- @changelog
 --   Quick preset: vocals only.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
