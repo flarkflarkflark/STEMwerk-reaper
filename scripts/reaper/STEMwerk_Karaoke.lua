da78c51fc5805ce02b7b57f2d4fdfe6d51b8d1b9
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-26 16:29:35 +0200

    reapack: publish support summary hotfix 2.2.2.2.5

--- scripts/reaper/STEMwerk_Karaoke.lua
+++ scripts/reaper/STEMwerk_Karaoke.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Karaoke
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.2.4
+-- @version 2.2.2.2.5
 -- @changelog
 --   Quick preset: vocals removed.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
