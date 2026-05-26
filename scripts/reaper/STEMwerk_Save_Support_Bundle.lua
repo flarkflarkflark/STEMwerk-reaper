8054dae5a9047125365eadf369106c2afdfdb806
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-26 06:25:11 +0200

    reapack: publish support stabilization 2.2.2.2.3

--- scripts/reaper/STEMwerk_Save_Support_Bundle.lua
+++ scripts/reaper/STEMwerk_Save_Support_Bundle.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Save Support Bundle
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.2.2
+-- @version 2.2.2.2.3
 -- @changelog
 --   Collects a read-only STEMwerk support bundle with runtime diagnostics, logs, and temp-folder inventory.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
