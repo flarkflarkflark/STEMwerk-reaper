7bc0421f60cbbe3c9eda8eae82e706624e6eae75
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-27 16:11:23 +0200

    reapack: publish Intel Mac setup hotfix 2.2.2.2.6

--- scripts/reaper/STEMwerk_Karaoke.lua
+++ scripts/reaper/STEMwerk_Karaoke.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Karaoke
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.2.5
+-- @version 2.2.2.2.6
 -- @changelog
 --   Quick preset: vocals removed.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
