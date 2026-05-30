9a9c57751250218a4aa8a85619068ce0cdfe0862
flarkflarkflark <flarkflarkflark@gmail.com>
2026-05-30 11:32:31 +0200

    reapack: publish samplerate guard ordering hotfix 2.2.2.2.9

--- scripts/reaper/STEMwerk_All_Stems.lua
+++ scripts/reaper/STEMwerk_All_Stems.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: All Stems
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.2.8
+-- @version 2.2.2.2.9
 -- @changelog
 --   Quick preset: all stems.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
--- scripts/reaper/STEMwerk_Karaoke.lua
+++ scripts/reaper/STEMwerk_Karaoke.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: Karaoke
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.2.2.2.8
+-- @version 2.2.2.2.9
 -- @changelog
 --   Quick preset: vocals removed.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
