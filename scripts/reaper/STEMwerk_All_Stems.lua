edc63099cca925ad73b2475e7973447104a537d3
flarkflarkflark <flarkflarkflark@gmail.com>
2026-07-15 07:39:23 +0200

    release: prepare STEMwerk 2.3.0.5 planning

--- scripts/reaper/STEMwerk_All_Stems.lua
+++ scripts/reaper/STEMwerk_All_Stems.lua
@@ -1,6 +1,6 @@
 -- @description Stemwerk: All Stems
 -- @author flarkAUDIO <flarkaudio@pm.me>
--- @version 2.3.0.4
+-- @version 2.3.0.5
 -- @changelog
 --   Quick preset: all stems.
 -- @link Repository https://github.com/flarkflarkflark/STEMwerk
